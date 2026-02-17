4a3f435367a11090965046bb439e6e3fb9db532d
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-02-18 00:31:03 +0200

    deltamod default window size parity

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -37,5 +37,7 @@ public partial class MainWindow : Window
         MaximizeBtn.Click += (_, _) =>
             WindowState = (WindowState == WindowState.FullScreen) ? WindowState.Normal : WindowState.FullScreen;
         CloseWinBtn.Click += (_, _) => Close();
+        Width = Screens.All[0].Bounds.Width * 0.4;
+        Height = Screens.All[0].Bounds.Height * 0.7;
     }
 }
