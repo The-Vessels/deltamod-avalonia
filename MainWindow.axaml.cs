e29da91662fc92d64b01b6685b60208a8e02c42c
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-03-13 03:37:09 +0200

    Something beautiful

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -46,14 +46,5 @@ public partial class MainWindow : Window
         {
             Console.WriteLine(TitleBar.Bounds);
         };
-
-        Sidebar.PointerEntered += (_, _) =>
-        {
-            SidebarBackdrop.Background = new SolidColorBrush(Color.Parse("rgba(0, 0, 0, 0.5)"));
-        };
-        Sidebar.PointerExited += (_, _) =>
-        {
-            SidebarBackdrop.Background = new SolidColorBrush(Color.Parse("rgba(0, 0, 0, 0)"));
-        };
     }
 }
